0aa7ee5d54c519c4a0fddeee619e3f8d47c94874
Mr-Robot-101 <git.sujon@gmail.com>
2022-09-03 00:32:23 +0600

    sorting section UI done

--- JS/app.js
+++ JS/app.js
@@ -9,17 +9,24 @@ const findCategories = categories => {
     const categoriesHolder=document.getElementById("all-categories")
     categories.forEach(category => {
         // console.log(category.category_name)
-        const createCategory= document.createElement("p")
-        createCategory.classList.add("me-4")
-        createCategory.innerText=`${category.category_name}`
+        const createCategory= document.createElement("div")
+        createCategory.innerHTML=`<p onclick="loadNews(${category.category_id})" class="me-4">${category.category_name}</p>`
         categoriesHolder.append(createCategory)   
     });
-
 }
+
+const loadNews = async() => {
+     const url=`https://openapi.programming-hero.com/api/news/category/${01}`
+    const res= await fetch(url)
+    const data= await res.json()
+    console.log(data)
+}
+
+
 
 loadCategories()
 
-x= {
-    "category_id": "01",
-    "category_name": "Breaking News"
-}
+// x= {
+//     "category_id": "01",
+//     "category_name": "Breaking News"
+// }
